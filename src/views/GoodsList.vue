6104f2aa1e509fb482216f68dd6378c9b97ca15a
彭胜光-主机 <499964903@qq.com>
2018-01-29 07:34:34 +0800

    宿舍主机开发环境部署成功！

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -7,7 +7,7 @@
     <div class="accessory-result-page accessory-page">
       <div class="container">
         <div class="filter-nav">
-          <span class="sortby">Sort by:</span>
+          <span class="sortby"> Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
           <a @click="sortGoods" href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
