ac98c00d2ae35b8b4d59a969933a0525b3c01386
彭胜光-主机 <499964903@qq.com>
2018-05-06 23:48:13 +0800

    【个人信息】页面UI交互

--- src/components/personal/Personal.vue
+++ src/components/personal/Personal.vue
@@ -1,11 +1,188 @@
 <template>
-  <div>个人信息</div>
+  <div class="personal-wrapper">
+    <div class="avatar-box" :style="{background: backgroundImage}">
+      <div class="mask"></div>
+      <div class="myInfo">
+        <div class="avatar">
+          <img src="./../../assets/img/avatar/men1.png" alt="">
+        </div>
+        <div class="name">Lighter_Studio</div>
+        <div class="position">职位：人工智能工程师</div>
+        <div class="label-box">
+          <span class="label">90后</span>
+          <span class="label">珠海市</span>
+          <span class="label">魔蝎座</span>
+        </div>
+      </div>
+      <div class="edit">
+        <img src="./../../assets/img/personal/editIcon.png" alt="">
+        <span>编辑</span>
+      </div>
+    </div>
+    <Scroll class="container-wrapper">
+      <div class="main">
+        <tab-switch></tab-switch>
+        <!--<div class="area-box">-->
+          <!--<div class="head-box">个人设置</div>-->
+          <!--<div class="function-box">-->
+            <!--<div class="function">修改头像</div>-->
+            <!--<div class="function">修改头像</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="area-box">-->
+        <!--<div class="head-box">个人设置</div>-->
+        <!--<div class="function-box">-->
+          <!--<div class="function">修改头像</div>-->
+          <!--<div class="function">修改头像</div>-->
+        <!--</div>-->
+      <!--</div>-->
+        <!--<div class="area-box">-->
+          <!--<div class="head-box">个人设置</div>-->
+          <!--<div class="function-box">-->
+            <!--<div class="function">修改头像</div>-->
+            <!--<div class="function">修改头像</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="area-box">-->
+          <!--<div class="head-box">个人设置</div>-->
+          <!--<div class="function-box">-->
+            <!--<div class="function">修改头像</div>-->
+            <!--<div class="function">修改头像</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="area-box">-->
+          <!--<div class="head-box">个人设置</div>-->
+          <!--<div class="function-box">-->
+            <!--<div class="function">修改头像</div>-->
+            <!--<div class="function">修改头像</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      </div>
+    </Scroll>
+  </div>
 </template>
 
 <script>
-  export default {}
+  import Avatar from '@/base/ImageUpload/Avatar'
+  import Scroll from '@/base/scroll/Scroll'
+  import TabSwitch from './PersonalMain.vue'
+
+  export default {
+    components: {
+      Avatar,
+      Scroll,
+      TabSwitch
+    },
+    data () {
+      return {
+        backgroundImage: 'url(' + require('@/assets/img/swiper/Bubbly.png') + ')'
+      }
+    }
+  }
 </script>
 
-<style>
+<style lang="scss">
+  .personal-wrapper{
+    .avatar-box{
+    }
+    .container-wrapper{}
+  }
 
+</style>
+<style lang="scss" scoped>
+  .personal-wrapper{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    .avatar-box{
+      height: 210px;
+      width: 100%;
+      position: relative;
+      background-size: 100% 100%!important;
+      .mask{
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.4);
+      }
+      .myInfo{
+        position: absolute;
+        bottom: 22px;
+        left: 34px;
+        z-index: 201;
+        color: #fff!important;
+        .avatar{
+          padding-bottom: 4px;
+          img{
+            background-size: 100% 100%;
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+          }
+        }
+        .name{
+          font-size: 20px;
+          color: #fff;
+          font-weight: bold;
+        }
+        .position{
+          padding: 8px 0px;
+        }
+        .label-box{
+          .label{
+            font-size: 14px;
+            background-color: #989ea5;
+            border-radius: 30px;
+            padding: 2px 8px;
+          }
+        }
+      }
+      .edit{
+        position: absolute;
+        top: 50%;
+        right: 30px;
+        background-color: #989ea5;
+        display: flex;
+        padding: 4px 8px;
+        border-radius: 6px;
+        img{
+          width: 20px;
+          height: 20px;
+        }
+        span{
+          color: #fff;
+          padding-left: 6px;
+        }
+      }
+
+    }
+    .container-wrapper{
+      position: fixed;
+      top: 200px;
+      bottom: 52px;
+      overflow: auto;
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      border-radius: 14px;
+      .main{
+        height: 100%; /*这里限制了scroll不能滑动*/
+        .area-box{
+          .head-box{
+            background-color: #f0f4f7;
+            padding: 6px;
+          }
+          .function-box{
+            .function{
+              font-size: 20px;
+              padding: 10px 0 10px 6px;
+              border-bottom: 1px solid #605F5F;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
---
+++ src/components/personal/PersonalMain.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="personal-main-wrapper">
+    <tab :line-width=2 active-color='#d33a31' v-model="index">
+      <tab-item class="vux-center" :selected="demo2 === item" v-for="(item, index) in list2" @click="demo2 = item" :key="index">{{item}}</tab-item>
+    </tab>
+    <swiper v-model="index" :show-dots="false">
+      <swiper-item>
+        <div class="tab-swiper vux-center">1 Container</div>
+      </swiper-item>
+      <swiper-item >
+        <div class="tab-swiper vux-center">暂时还没有动态哦</div>
+      </swiper-item>
+      <swiper-item >
+        <div class="tab-swiper">
+          <div class="area-box">
+          <div class="head-box">个人信息</div>
+          <div class="function-box">
+            <div class="function">职业：人工智能工程师</div>
+            <div class="function">年龄：90后 魔蝎座</div>
+            <div class="function">地区：广东省 珠海市</div>
+          </div>
+        </div>
+        </div>
+        <div class="tab-swiper">
+          <div class="area-box">
+            <div class="head-box">个人信息</div>
+            <div class="function-box">
+              <div class="function">职业：人工智能工程师</div>
+              <div class="function">年龄：90后 魔蝎座</div>
+              <div class="function">地区：广东省 珠海市</div>
+            </div>
+          </div>
+        </div>
+        <div class="tab-swiper">
+          <div class="area-box">
+            <div class="head-box">个人介绍</div>
+            <div class="function-box">
+              <div class="function">还没有填写个人介绍</div>
+            </div>
+          </div>
+        </div>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+<script>
+  import { Tab, TabItem, Swiper, SwiperItem } from 'vux'
+  const list = () => ['设置', '动态', '关于我']
+
+  export default {
+    components: {
+      Tab,
+      TabItem,
+      Swiper,
+      SwiperItem
+    },
+    data () {
+      return {
+        list2: list(),
+        demo2: '美食',
+        index: 0
+      }
+    },
+    methods: {
+      onItemClick (index) {
+        console.log('on item click:', index)
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import '~vux/src/styles/1px.less';
+  @import '~vux/src/styles/center.less';
+
+  .box {
+    padding: 15px;
+  }
+  .tab-swiper {
+    padding-top: 10px;
+  }
+  .active-6-1 {
+    color: rgb(252, 55, 140) !important;
+    border-color: rgb(252, 55, 140) !important;
+  }
+  .active-6-2 {
+    color: #04be02 !important;
+    border-color: #04be02 !important;
+  }
+  .active-6-3 {
+    color: rgb(55, 174, 252) !important;
+    border-color: rgb(55, 174, 252) !important;
+  }
+</style>
+
+<style lang="scss">
+  .personal-main-wrapper{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    .vux-tab{}
+    .vux-slider{
+      overflow: auto;
+      flex: auto;
+      display: flex;
+      flex-direction: column;
+      .vux-swiper{
+        overflow: auto;
+        flex: auto;
+        .vux-swiper-item{
+          background-color: #f9f9f9;
+          .tab-swiper{
+            .area-box{
+              background-color: #f9f9f9;
+              .head-box{
+                font-size: 14px;
+                border-left: 2px solid #d33a31;
+                padding-left: 10px;
+                font-weight: 600;
+              }
+              .function-box{
+                /*border: 1px solid #009de6;*/
+                padding-top: 10px;
+                .function{
+                  font-size: 14px;
+                  padding: 1px 0 10px 12px;
+                  /*border-bottom: 1px solid #605F5F;*/
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
